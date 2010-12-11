0fb207fe38ccbe926ef398ebe772be3d2ba31d96
Sebastiaan Deckers <cbas@pandion.be>
2010-12-11 10:58:13 +0800

    Importing some shims

---
+++ libs/console.js
@@ -0,0 +1,16 @@
+﻿/**
+	This file is part of Web Client
+	@author Copyright (c) 2010 Sebastiaan Deckers
+	@license GNU General Public License version 3 or later
+*/
+if (!("console" in window)) {
+	window.console = {};
+}
+
+(	"assert count debug dir dirxml error group groupCollapsed groupEnd " +
+	"info log markTimeline profile profileEnd time timeEnd trace warn"
+).split(" ").forEach(function (api) {
+	if (!(api in window.console)) {
+		window.console[api] = function () {};
+	}
+});
---
+++ shim.js
@@ -0,0 +1,18 @@
+﻿/**
+	This file is part of ShimJS
+*/
+define(
+	[
+		"libs/global-es5.js",
+		"libs/split.js",
+		"libs/console.js",
+		"libs/json2.js",
+		"libs/DOMParser.js",
+		"libs/XMLHttpRequest.js",
+		"libs/HTMLElement.js",
+		"libs/classList.js"
+	],
+	function () {
+		return {};
+	}
+);
