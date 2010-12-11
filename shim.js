7d2a40df9c67f84c388d523c8949ebf63a6eee12
Sebastiaan Deckers <cbas@pandion.be>
2010-12-11 14:55:55 +0800

    Preliminary API

--- shim.js
+++ shim.js
@@ -1,18 +1,25 @@
 ﻿/**
 	This file is part of ShimJS
 */
-define(
-	[
-		"libs/global-es5.js",
-		"libs/split.js",
-		"libs/console.js",
-		"libs/json2.js",
-		"libs/DOMParser.js",
-		"libs/XMLHttpRequest.js",
-		"libs/HTMLElement.js",
-		"libs/classList.js"
-	],
-	function () {
-		return {};
-	}
-);
+(function (window) {
+	// Name of our public API as it is available in the browser as a global variable.
+	var publicInterface = "shimjs";
+
+	window[publicInterface] = function () {
+
+		var shims = [],
+			callback = (arguments[arguments.length] instanceof Function) ?
+						arguments[arguments.length] : new Function
+		;
+
+		// Build a list of the needed shims
+		for (var i = 0; i < argmuments.length; i++) {
+			if (typeof arguments[i] === "string") {
+				shims.push("libs/" + arguments[i]);
+			}
+		}
+
+		// Include the JS files and fire the callback when done
+		require(shims, callback);
+	};
+})(this);
